571c9e976f58f8348a4b548814a274a2cf8e8c38
Siddhant Kumar <siddhantgrooti@gmail.com>
2026-04-06 02:06:50 +0530

    Final Updates: Fix build error

--- src/components/CTAForm.tsx
+++ src/components/CTAForm.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import emailjs from '@emailjs/browser';
 
 export default function CTAForm() {
   const [email, setEmail] = useState('');
